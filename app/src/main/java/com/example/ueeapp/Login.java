ccee7dbd36ae9b7a0107f048e52f022080c5ab50
SManeesha99 <it20649012@my.sliit.lk>
2022-11-06 14:10:55 +0530

    Login Register Backend
    Login Register Backend

--- app/src/main/java/com/example/ueeapp/Login.java
+++ app/src/main/java/com/example/ueeapp/Login.java
@@ -2,13 +2,50 @@ package com.example.ueeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    EditText lgemail,lgpassword;
+    Button lglogin;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        lgemail = (EditText) findViewById(R.id.loginemailinput);
+        lgpassword = (EditText) findViewById(R.id.loginpassinput);
+        lglogin = (Button) findViewById(R.id.loginbtn);
+        DB =new DBHelper(this);
+
+        lglogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = lgemail.getText().toString();
+                String password = lgpassword.getText().toString();
+
+                if (email.equals("")||password.equals("")){
+                    Toast.makeText(Login.this, "Please Fill All Fields", Toast.LENGTH_LONG).show();
+                }
+                else{
+                    Boolean chechuserpass = DB.checkemailpassword(email,password);
+                    if (chechuserpass==true){
+                        Toast.makeText(Login.this, "Sign inSuccessfully", Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                        startActivity(intent);
+                    }
+                    else {
+                        Toast.makeText(Login.this, "Invalid Credentials", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
--- app/src/main/java/com/example/ueeapp/register.java
+++ app/src/main/java/com/example/ueeapp/register.java
@@ -2,13 +2,67 @@ package com.example.ueeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class register extends AppCompatActivity {
+
+    EditText rename,remobile,reemail,repassword,regrepassword;
+    Button register;
+    DBHelper DB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        rename = (EditText) findViewById(R.id.regNameinput);
+        remobile = (EditText) findViewById(R.id.regmobilnuminput);
+        reemail = (EditText) findViewById(R.id.regemailinput);
+        repassword = (EditText) findViewById(R.id.regpassinput);
+        regrepassword = (EditText) findViewById(R.id.regrepassinput);
+        register = (Button) findViewById(R.id.registerbtn);
+        DB = new DBHelper(this);
+
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String name = rename.getText().toString();
+                String mobile = remobile.getText().toString();
+                String email = reemail.getText().toString();
+                String password = repassword.getText().toString();
+                String repassword = regrepassword.getText().toString();
+
+                if (name.equals("")||email.equals("")||mobile.equals("")||password.equals("")||repassword.equals(""))
+                    Toast.makeText(register.this, "Please Enter ", Toast.LENGTH_SHORT).show();
+                else {
+                    if (password.equals(repassword)){
+                        Boolean checkuser = DB.checkemail(email);
+                        if (checkuser==false){
+                            Boolean insert = DB.insertData(name,mobile, email,password);
+                            if (insert==true){
+                                Toast.makeText(register.this, "Registered Successfully", Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(getApplicationContext(),Login.class);
+                                startActivity(intent);
+                            }
+                            else {
+                                Toast.makeText(register.this, "Regitration Failed", Toast.LENGTH_SHORT).show();
+                            }
+                        }
+                        else {
+                            Toast.makeText(register.this, "User Already Exists Please Sign in", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    else {
+                        Toast.makeText(register.this, "Password Not Matched", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            }
+        });
     }
 }
