7c386d156f1eb8054e05f0ee8a39baf568fedf25
Umesh Madushan <umeshmadushan665@gmail.com>
2022-11-06 15:43:06 +0530

    umesh
    umesh

---
+++ app/src/main/java/com/example/ueeapp/PostJob.java
@@ -0,0 +1,14 @@
+package com.example.ueeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostJob extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_job);
+    }
+}
---
+++ app/src/main/java/com/example/ueeapp/SendTenderProposal.java
@@ -0,0 +1,14 @@
+package com.example.ueeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SendTenderProposal extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_tender_proposal);
+    }
+}
